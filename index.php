a4b0407eeb3c4f32e9f6bdd6295ed5f163521135
Lilyyyc <124380100+Lilyyyc@users.noreply.github.com>
2023-03-07 20:06:40 +0000

    Code for index.php

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
 <!DOCTYPE html>
+<html lang="en">
+	<head>
+
+    <!-- Meta data -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="./css/style.css">
+
+    <!-- Web page title -->
+    <title>Hello, World! in PHP</title>
+	</head>
+	<body>
+
+    <!-- Web page content -->
+    <h1><?php echo 'There is something special about this web page...'; ?></h1>
+		<?php echo '<p>This is my first web page using PHP! I enjoyed creating this web page using an entirely new language, but one that I could use my knowledge of HTML to improve.</p>'; ?>
+	</body>
+</html>
